e2b7549a107db969f0aa6a95a4e15389866d8f91
Matheus Henrique Caraça da Silva <hmatheus23@hotmail.com>
2022-07-02 15:48:28 -0300

    Correção de cores e logos

--- app/src/main/java/com/appturma/LoginActivity.java
+++ app/src/main/java/com/appturma/LoginActivity.java
@@ -4,13 +4,29 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.StringRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private String apiPath = "http://10.0.0.2/siteturma/usuarios/listar/";
+    private JSONArray restulJsonArray;
+    private int logado = 0;
+    private String mensagem = "", strusuario = "", stremail = "";
+
     EditText edtUsuario,edtSenha;
     Button btnLogin;
 
@@ -22,6 +38,8 @@ public class LoginActivity extends AppCompatActivity {
         edtUsuario = findViewById(R.id.editTextUsuario);
         edtSenha = findViewById(R.id.editTextSenha);
         btnLogin = findViewById(R.id.btnLogin);
+
+        AndroidNetworking.initialize(getApplicationContext());
 
         btnLogin.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
@@ -36,16 +54,85 @@ public class LoginActivity extends AppCompatActivity {
                 }
 
                 else {
-                    Intent base = new Intent(getApplicationContext(),BaseMenu.class);
-                    startActivity(base);
-                    finish();
+                    sendApi();
                 }
             }
         });
-
-
-
     }
 
+    protected void sendApi(){
+        AndroidNetworking.post(apiPath)
+                .addBodyParameter("HTTP_ACCEPT","application/json")
+                .addBodyParameter("txtNomeUsuario",edtUsuario.getText().toString())
+                .addBodyParameter("txtSenhaUsuario",edtSenha.getText().toString())
+                .setTag("test")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsString(new StringRequestListener() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            // Se o objeto for diferente de nulo
+                            if (jsonObject != null){
+                                restulJsonArray = jsonObject.getJSONArray("RetornoDados");
+                                JSONObject jsonObj = null;
+                                for (int i=0; i < restulJsonArray.length();i++){
+                                    jsonObj = restulJsonArray.getJSONObject(i);
+                                    logado = jsonObj.getInt("plogado");
+                                }
+                                switch (logado){
+                                    case 1:
+                                        mensagem = "Bem vindo ao Sistema";
+                                        break;
+                                    case 2:
+                                        mensagem = "Usuário já está conectado";
+                                        break;
+                                }
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this)
+                                        .setTitle("Aviso")
+                                        .setMessage(mensagem)
+                                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                if (logado == 1){
+                                                    Intent base = new Intent(getApplicationContext(),BaseMenu.class);
+                                                    startActivity(base);
+                                                    finish();
+                                                }
+                                            }
+                                        });
+                                builder.create().show();
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+                        try {
+                            if (anError.getErrorCode() == 0){
+                                mensagem = "Problemas com a conexão!! \nTente Novamente.";
+                            }
+                            else {
+                                JSONObject jsonObject = new JSONObject(anError.getErrorBody());
+                                if (jsonObject.getJSONObject("RetornoDados").getInt("sucesso") == 0){
+                                    mensagem = "Usuário ou senha inválidos";
+                                }
+                            }
+                            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this)
+                                    .setTitle("Aviso")
+                                    .setMessage(mensagem)
+                                    .setPositiveButton("OK",null);
+                            builder.create().show();
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.d("BridgeUpdateService","error" + anError.getErrorCode() +anError.getErrorDetail());
+                    }
+                });
+    }
 
 }
