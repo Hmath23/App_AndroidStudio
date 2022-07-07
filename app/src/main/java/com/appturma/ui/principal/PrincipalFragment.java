b5b7a5ce294331c924b6e975d2b00854f154eb74
Matheus Henrique Caraça da Silva <hmatheus23@hotmail.com>
2022-07-06 22:12:50 -0300

    Correções na navegação do sistema.

--- app/src/main/java/com/appturma/ui/principal/PrincipalFragment.java
+++ app/src/main/java/com/appturma/ui/principal/PrincipalFragment.java
@@ -1,13 +1,22 @@
 package com.appturma.ui.principal;
 
+import androidx.annotation.IdRes;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
